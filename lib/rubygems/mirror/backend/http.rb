13594f75c96b36626bfb6d184cbc995875dec3da
ruohan.chen <crhan123@gmail.com>
2014-04-16 01:24:01 +0800

    still a bugfix

--- lib/rubygems/mirror/backend/http.rb
+++ lib/rubygems/mirror/backend/http.rb
@@ -17,13 +17,7 @@ module Gem
         # Fetch a source path under the base uri, and put it in the same or given
         # destination path under the base path.
         def fetch path, etag=nil
-
-          req = Net::HTTP::Get.new "/#{path}"
-          req.add_field 'If-None-Match', etag if etag
-
-          @http.request URI(from(path)), req do |resp|
-            return handle_response(resp, path, etag)
-          end
+          _fetch from(path), path, etag
         end
 
         def from *args
@@ -32,6 +26,17 @@ module Gem
 
         private
 
+        def _fetch uri, path, etag
+
+          req = Net::HTTP::Get.new "/#{path}"
+          req.add_field 'If-None-Match', etag if etag
+
+          @http.request URI(uri), req do |resp|
+            return handle_response(resp, path, etag)
+          end
+
+        end
+
         # Handle an http response, follow redirects, etc. returns true if a file was
         # downloaded, false if a 304. Raise Error on unknown responses.
         def handle_response(resp, path, etag)
@@ -39,7 +44,7 @@ module Gem
           when 304
             return [nil, get_etag(resp)]
           when 302
-            fetch resp['location'], path, etag
+            _fetch resp['location'], path, etag
           when 200
             [StringIO.new(resp.body), get_etag(resp)]
           when 403, 404
